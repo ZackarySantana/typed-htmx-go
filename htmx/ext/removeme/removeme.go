3b856717749c67a2938bf8d2d84bff812616b6f1
ZackarySantana <64446617+ZackarySantana@users.noreply.github.com>
2025-09-17 20:23:06 -0400

    feat(remove-me): update url

--- htmx/ext/removeme/removeme.go
+++ htmx/ext/removeme/removeme.go
@@ -15,14 +15,14 @@ import (
 //
 // Extension: [remove-me]
 //
-// [remove-me]: https://htmx.org/extensions/remove-me/
+// [remove-me]: https://github.com/bigskysoftware/htmx-extensions/blob/main/src/remove-me/README.md
 const Extension htmx.Extension = "remove-me"
 
 // RemoveMe removes the element after the specified interval.
 //
 // Extension: [remove-me]
 //
-// [remove-me]: https://htmx.org/extensions/remove-me/
+// [remove-me]: https://github.com/bigskysoftware/htmx-extensions/blob/main/src/remove-me/README.md
 func RemoveMe[T any](hx htmx.HX[T], after time.Duration) T {
 	return hx.Attr("remove-me", after.String())
 }
